ba6eb79a40f505d9c0370ca0412f3e66d07f4ab6
4DERT <4dertdk@gmail.com>
2021-01-05 21:06:59 +0100

    Initial commit

---
+++ print_song_tabs.py
@@ -0,0 +1,52 @@
+#/usr/bin/python
+
+import guitarpro
+
+file = 'Orion.gp5'
+# file = 'Stairway to Heaven.gp5'
+song = guitarpro.parse(file)
+
+selected_track = "J. Hetfield (Rhythm)"
+# selected_track = "Jimmy Page - Acoustic Guitar"
+measure_start = 72
+measure_stop = 72
+
+# for track in song.tracks:
+#     if track.name == selected_track:
+#         for measure in track.measures:
+#             if measure.number >= measure_start and measure.number <= measure_stop:
+#                 print(f"NEW MEASURE: {measure.number}")
+#                 for voice in measure.voices:
+#                     print("\tNEW VOICE")
+#                     for beat in voice.beats:
+#                         print(f"\t\tNEW BEAT: 1/{beat.duration.value}")
+#                         try:
+#                             # print(f"\t\t\t{beat.notes}")
+#                             for chord in beat.notes:
+#                                 print(f"\t\t\t{chord.value}, on string: {chord.string}")
+#                                 # print(chord)
+#                         except:
+#                             pass
+
+
+print(song.tracks[0].name)
+for measure in song.tracks[0].measures:
+    if measure.number >= measure_start and measure.number <= measure_stop:
+        print(f"NEW MEASURE: {measure.number},\
+             measure.timeSignature.beams = {measure.timeSignature.beams}")
+        
+        for voice in measure.voices:
+            print(f"\tNEW VOICE")
+            for beat in voice.beats:
+                print(f"\t\tNEW BEAT: 1/{beat.duration.value}")
+                try:
+                    # print(f"\t\t\t{beat.notes}")
+                    for chord in beat.notes:
+                        print(f"\t\t\t {chord.type}")
+                        print(f"\t\t\t {chord.beat.duration.isDotted}")
+                        x = chord.effect.palmMute
+                        print(f"\t\t\t {x}")
+                        print(f"\t\t\t {chord.value}, on string: {chord.string}")
+                        # print(chord)
+                except:
+                    pass
---
+++ song_info.py
@@ -0,0 +1,36 @@
+#/usr/bin/python
+
+import guitarpro
+
+file = 'Orion.gp5'
+# file = 'Stairway to Heaven.gp5'
+# file = 'Deutschland.gp5'
+song = guitarpro.parse(file)
+
+def getTuning(obj):
+    """
+    Return an array of given track tuning
+    """
+    # return [str(string) for string in obj.strings]
+    return [string for string in obj.strings]
+
+
+def tracksInfo(song):
+    for i, track in enumerate(song.tracks):
+        tuning = getTuning(track)[::-1]
+        # tuning = ', '.join(string for string in tuning)
+        print(f"{i}. {track.name}")
+        print(f"   {track.channel}")
+        print('   -> drum: ', len(track.strings)) if track.isPercussionTrack else print('   -> strings: ', len(track.strings), '\n', '  ->',tuning )
+        print()
+
+
+if __name__ == "__main__":
+    print(f"File: {file}")
+    print(f"Song title: {song.title}, from album: {song.album}")
+    print(F"Song artist: {song.artist}")
+    print(F"Song tempo: {song.tempo}")
+    print(F"Song key: {song.key}")
+    print(f"Song have {len(song.tracks)} tracks:")
+    tracksInfo(song)
+    
